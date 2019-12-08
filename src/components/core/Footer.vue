0fbfe89619aa7c8aef1f33027d29e9afe21a3c0f
adriantelles <awesomewebdev01@gmail.com>
2019-12-08 22:35:42 +1000

    asd

--- src/components/core/Footer.vue
+++ src/components/core/Footer.vue
@@ -1,45 +1,20 @@
 <template>
-  <v-footer
-    id="core-footer"
-    absolute
-    height="82"
-  >
+  <v-footer id="core-footer" absolute height="82">
     <div class="footer-items">
-      <span
-        v-for="link in links"
-        :key="link.name"
-      >
-        <a
-          :href="link.Link"
-          class="tertiary--text footer-links">{{ link.name }}</a>
+      <span v-for="link in links" :key="link.name">
+        <a :href="link.Link" class="tertiary--text footer-links">{{ link.name }}</a>
       </span>
     </div>
-    <v-spacer/>
-    <span class="font-weight-light copyright">
-      &copy;
-      {{ (new Date()).getFullYear() }}
-      <a
-        href="https://mvp.microsoft.com/en-us/PublicProfile/37586"
-        target="_blank">Ashraf Alam</a>, made with
-      <v-icon
-        color="tertiary"
-        size="17">mdi-heart</v-icon>
-      for a better web
-    </span>
+    <v-spacer />
   </v-footer>
 </template>
 
 <script>
 export default {
   data: () => ({
-    links: [
-      { name: 'Home', Link: '/dashboard' },
-      { name: 'Ashraf Alam', Link: 'https://mvp.microsoft.com/en-us/PublicProfile/37586' },
-      { name: 'Github', Link: 'https://github.com/AshrafAlam' },
-      { name: 'Stackoverflow', Link: 'https://stackoverflow.com/users/79739/ashraf-alam' }
-    ]
+    links: [{ name: 'Home', Link: '/dashboard' }]
   })
-}
+};
 </script>
 
 <style>
